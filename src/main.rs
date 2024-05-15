7d54f1e15f858a966d6dfb825a02a1aed153a945
anas jaidi <anas.jaidi@icloud.com>
2024-05-15 22:09:41 +0100

    chore: into into command parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn pad_string(input: String, size: usize) -> String {
+fn pad_string(input: &str, size: usize) -> String {
     if input.len() > size {
         format!("{}...", &input[..size - 3])
     } else {
@@ -6,6 +6,7 @@ fn pad_string(input: String, size: usize) -> String {
     }
 }
 
+#[derive(Debug, Clone)]
 enum TodoStatus {
     Backlog,
     Todo,
@@ -37,6 +38,7 @@ impl TodoStatus {
     }
 }
 
+#[derive(Debug, Clone)]
 struct Todo {
     id: u32,
     name: String,
@@ -96,15 +98,78 @@ impl Todo {
             .collect();
     }
 
-    fn display(&self, sep: char, pad_size: usize) {}
+    fn display(&self, sep: char, pad_size: usize) {
+        let description = self
+            .description
+            .as_ref()
+            .map_or("None".to_owned(), |x| x.clone());
+
+        let print_fields = |f: &str, s: Option<char>| -> String {
+            if s.is_some() {
+                format!("{}{}", pad_string(f, pad_size), s.unwrap())
+            } else {
+                format!("{}", pad_string(f, pad_size))
+            }
+        };
+
+        print!("{}", print_fields(self.id.to_string().as_str(), Some(sep)));
+        print!("{}", print_fields(self.name.as_str(), Some(sep)));
+        print!("{}", print_fields(self.status.format().as_str(), Some(sep)));
+        print!("{}", print_fields(description.as_str(), Some(sep)));
+        println!(
+            "{}",
+            print_fields(format!("{:?}", self.dependencies).as_str(), None)
+        );
+    }
 }
 
 struct TasksManager {
     tasks: Vec<Todo>,
 }
 
-impl TasksManager {}
+impl TasksManager {
+    fn new() -> Self {
+        Self { tasks: vec![] }
+    }
 
-fn main() {
-    println!("Hello, world!");
+    fn add(&mut self, t: &Todo) {
+        self.tasks.push(t.clone());
+    }
+
+    fn remove(&mut self, id: usize) -> Option<Todo> {
+        if let Some(pos) = self.tasks.iter().position(|x| x.id == id as u32) {
+            Some(self.tasks.remove(pos))
+        } else {
+            None
+        }
+    }
+
+    fn update() -> Option<Todo> {
+        None
+    }
 }
+
+fn parse_command(cmd: String) -> Result<Vec<String>, String> {
+    let command: Vec<String> = cmd.split(' ').into_iter().map(|s| s.to_owned()).collect();
+    match command[0].as_str() {
+        "show" => {
+            let possible_args = ["--all", "--id"];
+            let args = (&command[1..]).to_vec();
+            let mut err = false;
+            (&args).iter().for_each(|x| {
+                err = true;
+            });
+            Ok(args)
+        }
+        "add" => Ok(vec![]),
+        "remove" => Ok(vec![]),
+        "update" => Ok(vec![]),
+        _ => Err(String::new() + "Failed to handle command"),
+    }
+}
+
+fn handle_command(cmd: Vec<String>) -> Result<Todo, String> {
+    Err(String::new() + "Failed to handle command")
+}
+
+fn main() {}
