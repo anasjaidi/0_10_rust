f9ca2845fb6d1e4300fb60aba8b8685c26fe2a5b
anas jaidi <anas.jaidi@icloud.com>
2024-05-17 03:00:38 +0100

    chore: more into parsingW

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,62 @@ enum ParsingError<'a> {
     InvalidArgument(&'a String),
 }
 
-impl ParsingError<'_> {
+impl<'a> ParsingError<'a> {
     fn handle(&self) {
         match self {
-            _ => {}
-        };
+            ParsingError::EmptyLine => eprintln!("Error: Empty line"),
+            ParsingError::UnexpectedToken(token) => eprintln!("Unexpected token: {}", token),
+            ParsingError::InvalidModifier(modifier) => eprintln!("Invalid modifier: {}", modifier),
+            ParsingError::InvalidCommand(cmd) => eprintln!("Invalid command: {}", cmd),
+            ParsingError::InvalidArgument(arg) => eprintln!("Invalid argument: {}", arg),
+        }
     }
+}
+
+fn parse_args<'a>(
+    tokens: &'a Vec<String>,
+    accept_args: bool,
+    modofiers: Vec<&str>,
+    possible_args: Vec<&str>,
+) -> Result<(Vec<Argument>, Vec<String>), ParsingError<'a>> {
+    if !accept_args && !tokens.is_empty() {
+        return Err(ParsingError::UnexpectedToken(&tokens[0]));
+    };
+
+    let is_argument = |s: &String| -> Option<Argument> {
+        // Split the input string into key and value parts on the first '=' character.
+        let parts: Vec<&str> = s.splitn(2, '=').collect();
+        // Ensure that we have exactly two parts (key and value).
+        if parts.len() == 2 {
+            // Trim whitespace from both key and value.
+            let key = parts[0].trim();
+            if !possible_args.contains(&key) {
+                return None
+            }
+            let value = parts[1].trim();
+            // Create and return the Argument.
+            Some(Argument::new(key.to_string(), value.to_string()))
+        } else {
+            // If the input string doesn't contain '=', it's not a valid argument.
+            None
+        }
+    }
+    let is_modifier = |s: &String| modofiers.contains(&s.as_str());
+
+    let mut modifiers = vec![];
+    let mut args = vec![];
+
+    for t in tokens.iter() {
+        if let Some(arg) = is_argument(t) {
+            args.push(arg);
+        } else if is_modifier(t) {
+            modifiers.push(t.to_owned());
+        } else {
+            return Err(ParsingError::UnexpectedToken(t));
+        }
+    }
+
+    Ok((args, modifiers))
 }
 
 fn parse_exit(tokens: &Vec<String>) -> Result<(), ParsingError> {
@@ -48,12 +98,6 @@ fn parse_exit(tokens: &Vec<String>) -> Result<(), ParsingError> {
         Ok(())
     }
 }
-
-fn parse_show(tokens: &Vec<String>) -> Result<(), ParsingError> {}
-fn parse_add(tokens: &Vec<String>) -> Result<(), ParsingError> {}
-fn parse_remove(tokens: &Vec<String>) -> Result<(), ParsingError> {}
-fn parse_update(tokens: &Vec<String>) -> Result<(), ParsingError> {}
-
 fn parse_line(line: &String) -> Result<Command, ParsingError> {
     if line.len() == 0 {
         return Err(ParsingError::EmptyLine);
