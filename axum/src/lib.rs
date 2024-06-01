5a5e6501a360bc9f5c1d9d5b4c3b836b2b076a32
anas jaidi <anas.jaidi@icloud.com>
2024-06-01 23:58:15 +0100

    chore:  test new feat

--- axum/src/lib.rs
+++ axum/src/lib.rs
@@ -1,12 +1,18 @@
-use axum::{http::Method, Extension, Router};
+use axum::{http::Method, routing, Extension, Router};
 use std::{error::Error, net::SocketAddr};
 use tower_http::cors::{AllowMethods, Any, CorsLayer};
+use utils::http_reponse::{error::ErrorHttpResponse, success::SuccessHttpResponse};
 mod auth;
 mod manga;
+mod utils;
 
 #[derive(Default, Clone)]
 pub struct Shared {
     pub database: (),
+}
+
+async fn test() -> Result<SuccessHttpResponse<String>, ErrorHttpResponse> {
+    Ok(SuccessHttpResponse::default().change_code(201u16))
 }
 
 pub async fn run() -> Result<(), Box<dyn Error>> {
@@ -16,6 +22,7 @@ pub async fn run() -> Result<(), Box<dyn Error>> {
     let shared_state = Shared::default();
 
     let router = Router::new()
+        .route("/test", routing::get(test))
         .nest("/manga", manga::router())
         .nest("/auth", auth::router())
         .layer(Extension(shared_state));
